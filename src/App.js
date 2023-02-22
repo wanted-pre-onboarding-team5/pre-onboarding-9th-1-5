99fb77a36a5adfeb39460c783b13c9e28e7ca921
eunoo1995 <eunoo1995@gmail.com>
2023-02-22 14:00:43 +0900

    [#3] Feat: react-router 라우팅 로직 구현
    index : 라우팅을 사용하기 위해 BrowserRouter 랩핑
    App : 페이지 라우팅 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import { SignInPage, SignUpPage } from 'pages';
+import { Main } from 'layouts';
+
 import React from 'react';
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <Routes>
+      <Route path='/' element={<Main />}>
+        <Route path='signin' element={<SignInPage />} />
+        <Route path='signup' element={<SignUpPage />} />
+      </Route>
+    </Routes>
   );
 }
 

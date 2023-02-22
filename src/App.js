8646fca95bf9f342177e8095e1b300d1db681f32
letsjo <hyunoh.jo@gmail.com>
2023-02-22 12:37:48 +0900

    [#3] Feat: Router 셋팅

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { lazy } from 'react';
+import { Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const Login = lazy(() => import('pages/Login'));
+const Join = lazy(() => import('pages/Join'));
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/signin' element={<Login />} />
+          <Route path='/signup' element={<Join />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 

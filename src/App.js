6f96b9b08404bad3f3748cf3533139f42b456e49
junsu1220 <crab7157@gmail.com>
2023-02-22 13:11:56 +0900

    [#3] Feat: Route 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Suspense, lazy, Fragment } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+
 function App() {
+  const Home = lazy(() => import('./pages/Home'));
+  const Signin = lazy(() => import('./pages/Signin'));
+  const Signup = lazy(() => import('./pages/Signup'));
+  const Todo = lazy(() => import('./pages/Todo'));
+
   return (
-    <>
-      <div></div>
-    </>
+    <Fragment>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/todo' element={<Todo />} />
+        </Routes>
+      </Suspense>
+    </Fragment>
   );
 }
 

5558fb8a4cf6e5263d4f7d4a5515f3df546c4e7d
seunghoon <tmdgns4321@gmail.com>
2023-02-23 03:28:49 +0900

    [#4] Feat: todo page 작성

--- src/pages/Todo/index.js
+++ src/pages/Todo/index.js
@@ -1,8 +1,31 @@
+import { getTodos } from 'apis/todoApi';
+import AddTodoItem from 'components/AddTodo';
+import TodoItem from 'components/TodoItem';
+import React, { useEffect, useState } from 'react';
+
 export const Todo = () => {
+  const [todos, setTodos] = useState([]);
+
+  const todoLists = async () => {
+    const todoLists = await getTodos();
+    setTodos(todoLists);
+  };
+
+  useEffect(() => {
+    todoLists();
+  }, []);
+
   return (
     <>
       <div>Todo</div>
-      <div></div>
+      <AddTodoItem setTodos={setTodos} todos={todos} />
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <TodoItem todo={todo} />
+          </div>
+        );
+      })}
     </>
   );
 };

5b1d6c69d951ed48a52b2c9f9bb5809902d43c73
Jiwoon <kfromh0136@Naver.com>
2023-02-22 13:42:19 +0900

    [#3] Feat: API 설정
    조건에 명시된 API URL을 하나의 js 파일로 정리했습니다.
    
    API 명시 내용은 다음과 같습니다.
    BASE_URL = 'https://pre-onboarding-selection-task.shop'
      signup: `${BASE_URL}/auth/signup`,
      signin: `${BASE_URL}/auth/signin`,
      createTodo: `${BASE_URL}/todos`,
      getTodos: `${BASE_URL}/todos`,
      updateTodo: `${BASE_URL}/todos`,
      deleteTodo: `${BASE_URL}/todos`,

---
+++ src/API.js
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://pre-onboarding-selection-task.shop';
+const API = {
+  signup: `${BASE_URL}/auth/signup`,
+  signin: `${BASE_URL}/auth/signin`,
+  createTodo: `${BASE_URL}/todos`,
+  getTodos: `${BASE_URL}/todos`,
+  updateTodo: `${BASE_URL}/todos`,
+  deleteTodo: `${BASE_URL}/todos`,
+};
+export default API;

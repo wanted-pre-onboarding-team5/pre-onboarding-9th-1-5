33b736ff5d077f72038cb6b5c814fd188f520466
junsu1220 <crab7157@gmail.com>
2023-02-23 06:41:11 +0900

    [#4] Fix: todoApi baseURL 변경

--- src/apis/todoApi.js
+++ src/apis/todoApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { API_END_POINT } from 'constants';
 
 const axiosConfig = {
-  baseURL: `${API_END_POINT}/auth`,
+  baseURL: `${API_END_POINT}`,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',

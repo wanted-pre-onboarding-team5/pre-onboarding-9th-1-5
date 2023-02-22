35af491a0ee804d1d6be06a8578d4cfcb3523498
Seok-CH <tjr6677@gmail.com>
2023-02-22 18:57:35 +0900

    [#4] Feat: todo 읽고 등록 기능 추가
    - todoApi 연결, 주요하게 살펴볼 부분은 토큰 전달 부분입니다. todoApi는 토큰 전달을 꼭해야합니다
    - todo 페이지의 구성품인 TodoForm과 TodoItem 컴포넌트 추가
    - useLoaderData()로 loader에 있는 data 받아서 일차적 렌더링에 사용됨

--- src/pages/Todo/index.js
+++ src/pages/Todo/index.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { TodoForm, TodoItem } from './components';
+
 export const Todo = () => {
+  const { data } = useLoaderData();
+  const [todoList, setTodoList] = React.useState(data);
+
   return (
     <>
       <div>Todo</div>
-      <div></div>
+      <TodoForm todoList={todoList} setTodoList={setTodoList} />
+      {todoList.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
     </>
   );
 };

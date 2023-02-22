808e2dd42c230c4417b626d692bc8a251401e934
junsu1220 <crab7157@gmail.com>
2023-02-23 06:45:50 +0900

    [#4] Feat: useTodo Authorization 추가

--- src/hooks/useTodo.jsx
+++ src/hooks/useTodo.jsx
@@ -1,18 +1,35 @@
 import { useEffect, useState } from 'react';
-import { todoInstance } from '../apis';
+import { todoInstance } from '../apis/todoApi';
+import { USER_TOKEN_KEY } from 'constants';
 
 export default function useTodo() {
   const [todoList, setTodoList] = useState([]);
 
   useEffect(() => {
-    const response = async () =>
-      await todoInstance.get('/todos').then(({ data }) => setTodoList([...data]));
+    const response = async () => {
+      try {
+        todoInstance.defaults.headers.Authorization = `Bearer ${localStorage.getItem(
+          USER_TOKEN_KEY,
+        )}`;
+        await todoInstance.get('/todos').then(({ data }) => setTodoList([...data]));
+      } catch (error) {
+        alert(error);
+      }
+    };
     response();
   }, []);
 
   const postTodo = async (todo) => {
-    const { data } = await todoInstance.post('/todos', { todo });
-    setTodoList([...todoList, data]);
+    try {
+      todoInstance.defaults.headers.Authorization = `Bearer ${localStorage.getItem(
+        USER_TOKEN_KEY,
+      )}`;
+      todoInstance.defaults.headers.contentType = 'application/json';
+      const { data } = await todoInstance.post('/todos', { todo });
+      setTodoList([...todoList, data]);
+    } catch (error) {
+      alert(error);
+    }
   };
 
   return { todoList, postTodo };

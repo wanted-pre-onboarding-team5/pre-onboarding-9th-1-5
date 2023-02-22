4aaf648fefab96782f2359aa8f6264771bb19981
letsjo <hyunoh.jo@gmail.com>
2023-02-23 03:36:27 +0900

    [#4] Refactor: Class 로 변경

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,32 +1,32 @@
 import { MESSAGE } from 'constants';
 import { handleError } from './handleError';
 
-export default function Validator() {
-  this.validate = (condition, errorMsg) => {
+export default class validator {
+  static #validate(condition, errorMsg) {
     if (condition) {
       return true;
     }
 
     return handleError(errorMsg);
-  };
+  }
 
-  this.isValidTodo = function (todo) {
+  static isValidTodo(todo) {
     return this.isExistTodo(todo) && this.isStringTodo(todo);
-  };
+  }
 
-  this.isExistTodo = function (todo) {
-    return this.validate(this.isSet(todo), MESSAGE.error.emptyTodo);
-  };
+  static isExistTodo(todo) {
+    return this.#validate(this.isSet(todo), MESSAGE.error.emptyTodo);
+  }
 
-  this.isStringTodo = function (todo) {
-    return this.validate(this.isString(todo), MESSAGE.error.notStringTodo);
-  };
+  static isStringTodo(todo) {
+    return this.#validate(this.isString(todo), MESSAGE.error.notStringTodo);
+  }
 
-  this.isSet = function (value) {
+  static isSet(value) {
     return value.trim().length > 0;
-  };
+  }
 
-  this.isString = function (value) {
+  static isString(value) {
     return typeof value === 'string';
-  };
+  }
 }

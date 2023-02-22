09fb48e7a411b3cf16f478eeaf0b046357ab3968
Seok-CH <tjr6677@gmail.com>
2023-02-22 13:49:12 +0900

    [#3] Feat: SignIn, SignUp 페이지 구현
    features폴더의 form기능을 가져와 각 페이지에서 활용하였습니다. 각페이지는 회원가입과 로그인페이지로 이동할 수 있는 링크가 있습니다.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { SignInForm } from 'features';
+import { Link } from 'react-router-dom';
 
 function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <div>
+        <h1>로그인</h1>
+        <SignInForm />
+        <div>
+          <span>아직 계정이 없나요? </span>
+          <Link to='/signup'>회원가입하기</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { SignUpForm } from 'features';
+import { Link } from 'react-router-dom';
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <div>
+        <h1>회원가입</h1>
+        <SignUpForm />
+        <div>
+          <span>이미 계정이 있나요? </span>
+          <Link to='/signin'>회원가입하기</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;

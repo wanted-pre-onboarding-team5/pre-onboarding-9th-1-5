309a8ed1481e75a6e8f18d41f268d451e9d87b44
eunoo1995 <eunoo1995@gmail.com>
2023-02-23 21:25:32 +0900

    [#21] Refactor: 로그인/회원가입 공통 view 컴포넌트화
    로그인과 회원가입의 view가 동일하여 컴포넌트로 분리하여 재사용성을 높였습니다.
    구현한 커스텀 훅을 활용하여 해당 페이지에 필요한 상태 및 핸들러를 사용하였습니다.
    AuthForm의 type prop에 따라 조건부로 알맞은 view가 그려지도록 구현하였습니다.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,51 +1,16 @@
-import { useCheckAccount } from 'hooks/useCheckAccount';
-import { useMovePage } from 'hooks/useMovePage';
-import { postSignIn } from 'apis/auth';
-import { USER_TOKEN_KEY } from 'constants';
+import { useForm } from 'hooks/useForm';
+import { AuthForm } from 'components/AuthForm';
 
 export const SignIn = () => {
-  const {
-    isButtonDisabled,
-    emailRef,
-    passwordRef,
-    emailInput,
-    passwordInput,
-    handleEmailChange,
-    handlePasswordChange,
-  } = useCheckAccount();
-
-  const [goTodo, goSignUp] = useMovePage(['/todo', '/signup']);
-
-  const handleSignIn = async (e) => {
-    e.preventDefault();
-    const { accessToken } = await postSignIn({ email: emailInput, password: passwordInput });
-    if (!accessToken) return;
-    localStorage.setItem(USER_TOKEN_KEY, accessToken);
-    goTodo();
-  };
+  const { formInputs, handleChange, handleSubmit, isValid } = useForm('signin');
 
   return (
-    <>
-      <h1>로그인</h1>
-      <form onSubmit={handleSignIn}>
-        <input
-          data-testid='email-input'
-          ref={emailRef}
-          value={emailInput}
-          onChange={handleEmailChange}
-        />
-        <input
-          data-testid='password-input'
-          ref={passwordRef}
-          value={passwordInput}
-          onChange={handlePasswordChange}
-          type='password'
-        />
-        <button type='submit' data-testid='signin-button' disabled={isButtonDisabled}>
-          로그인하기
-        </button>
-      </form>
-      <button onClick={goSignUp}>회원가입하러 가기</button>
-    </>
+    <AuthForm
+      type='signin'
+      formInputs={formInputs}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+      isValid={isValid}
+    />
   );
 };

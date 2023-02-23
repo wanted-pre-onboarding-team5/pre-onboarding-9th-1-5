dc69065778f0896d1e20cad18e115a038b1447c3
letsjo <hyunoh.jo@gmail.com>
2023-02-24 07:03:02 +0900

    [#22] Refactor: 속성 상수값 처리

--- src/components/public/Button/index.js
+++ src/components/public/Button/index.js
@@ -1,10 +1,19 @@
-const button = ({ ButtonData }) => {
-  const { text, testId, handleClick, data } = ButtonData;
+import { useEffect } from 'react';
+
+const Button = ({ props }) => {
+  const { text, testId, handleClick, userData, handleActive, active } = props;
+
+  if (handleActive) useEffect(handleActive, [userData]);
+
   return (
-    <button data-testid={testId} onClick={(e) => handleClick({ e, data })}>
+    <button
+      data-testid={testId}
+      onClick={(e) => handleClick({ e, userData })}
+      disabled={active ? false : true}
+    >
       {text}
     </button>
   );
 };
 
-export default button;
+export default Button;

443f27a7c6c0b26cfdd75b4d92b4810f09dce6c4
Jayun <jay.lee.2280@gmail.com>
2023-02-23 22:18:03 +0900

    [#22] Refactor: todoLoader 반환을 todoInstance를 활용하는 것으로 변경

--- src/pages/Todo/loader.js
+++ src/pages/Todo/loader.js
@@ -1,19 +1,12 @@
-import axios from 'axios';
 import { redirect } from 'react-router-dom';
 
 import { USER_TOKEN_KEY } from 'constants';
+import { todoInstance } from 'apis/todoApi';
 
 export const todoLoader = () => {
   const userToken = localStorage.getItem(USER_TOKEN_KEY);
   if (!userToken) {
     return redirect('/signin');
   }
-  return axios.get('', {
-    baseURL: `${process.env.REACT_APP_API_URL}/todos`,
-    timeout: 10000,
-    headers: {
-      Authorization: `Bearer ${userToken}`,
-      'Content-Type': 'application/json',
-    },
-  });
+  return todoInstance.get('');
 };

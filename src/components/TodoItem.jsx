587c074afe1f8287ebb4c8e1b942aa61c31d7af8
eunoo1995 <eunoo1995@gmail.com>
2023-02-23 14:54:06 +0900

    [#5] Feat: TodoItem 추가 삭제 기능 추가
    커스텀 훅을 사용한 상태 로직 분리 및 추가 삭제 버튼  조건부 렌더링 추가

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,53 @@
-export const TodoItem = ({ todo }) => {
+import { useTodo } from '../hooks/useTodo';
+
+export const TodoItem = ({ todo, setTodos }) => {
+  const {
+    isEditMode,
+    inputValue,
+    handleText,
+    handleChecked,
+    handleEditMode,
+    handleSubmit,
+    handleDelete,
+  } = useTodo(todo, setTodos);
+
   return (
     <li>
-      <label>
-        <input type='checkbox' checked={todo.isCompleted} onChange={() => {}} />
-        <span>{todo.todo}</span>
-      </label>
-      <button data-testid='modify-button'>수정</button>
-      <button data-testid='delete-button'>삭제</button>
+      {!isEditMode ? (
+        <>
+          <label>
+            <input
+              type='checkbox'
+              className='sr-only'
+              checked={todo.isCompleted}
+              onChange={handleChecked}
+            />
+            <span>{todo.todo}</span>
+          </label>
+          <button data-testid='modify-button' onClick={handleEditMode}>
+            수정
+          </button>
+          <button data-testid='delete-button' onClick={handleDelete}>
+            삭제
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            type='text'
+            data-testid='modify-input'
+            autoComplete='off'
+            value={inputValue}
+            onChange={handleText}
+          />
+          <button data-testid='submit-button' onClick={handleSubmit}>
+            완료
+          </button>
+          <button data-testid='cancel-button' onClick={handleEditMode}>
+            취소
+          </button>
+        </>
+      )}
     </li>
   );
 };

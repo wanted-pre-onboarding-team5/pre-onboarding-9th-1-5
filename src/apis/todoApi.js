1bc0fc0ed7f15be220e061933b1030a4aa6bf745
letsjo <hyunoh.jo@gmail.com>
2023-02-23 14:50:25 +0900

    [#5] Feat: todo update 및 delete API 구현

--- src/apis/todoApi.js
+++ src/apis/todoApi.js
@@ -20,7 +20,7 @@ todoInstance.interceptors.request.use((config) => {
 export const createTodo = async (todo) => {
   try {
     const response = await todoInstance.post('', todo);
-    return response.data;
+    return response;
   } catch (error) {
     console.error(error);
   }
@@ -29,7 +29,25 @@ export const createTodo = async (todo) => {
 export const getTodos = async () => {
   try {
     const response = await todoInstance.get();
-    return response.data;
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateTodo = async ({ id, todo, isCompleted }) => {
+  try {
+    const response = await todoInstance.put(`/${id}`, { todo, isCompleted });
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteTodo = async ({ id }) => {
+  try {
+    const response = await todoInstance.delete(`/${id}`);
+    return response;
   } catch (error) {
     console.error(error);
   }

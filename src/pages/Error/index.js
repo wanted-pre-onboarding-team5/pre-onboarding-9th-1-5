35fc8ae5c7edb6b5c8a56c9e960e2b8cf0948c42
letsjo <hyunoh.jo@gmail.com>
2023-02-24 06:54:17 +0900

    [#21] Feat: Error 페이지 컴포넌트 처리 및 상수값 처리

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,28 +1,52 @@
 import { useNavigate, useRouteError } from 'react-router-dom';
 import { useMovePage } from 'hooks/useMovePage';
 import { USER_TOKEN_KEY } from 'constants';
+import { PATH } from 'constants';
+import { MESSAGE } from 'constants';
+import Button from 'components/public/Button';
+import { PROPERTY } from 'constants';
 
 export const Error = () => {
   const error = useRouteError();
   const navigate = useNavigate();
   const userToken = localStorage.getItem(USER_TOKEN_KEY);
-  const [goSignIn, goSignUp, goTodo] = useMovePage(['/signin', '/signup', '/todo']);
-  console.error(error);
+  const [goSignIn, goSignUp, goTodo] = useMovePage([PATH.login, PATH.join, PATH.todo]);
 
   return (
     <div>
       <h2>{error.status}</h2>
       <h4>{error.statusText || error.message}</h4>
-      <h5>에러가 발생했어요!</h5>
+      <h5>{MESSAGE.error.occurError}</h5>
       {userToken ? (
-        <button onClick={goTodo}>할 일 보러 가기</button>
+        <Button
+          props={{
+            ...PROPERTY.button.goTodo,
+            handleClick: goTodo,
+          }}
+        />
       ) : (
         <>
-          <button onClick={goSignIn}>로그인하러 가기</button>
-          <button onClick={goSignUp}>회원가입하러 가기</button>
+          <Button
+            props={{
+              ...PROPERTY.button.goSignIn,
+              handleClick: goSignIn,
+            }}
+          />
+          <Button
+            props={{
+              ...PROPERTY.button.goSignUp,
+              handleClick: goSignUp,
+            }}
+          />
         </>
       )}
-      <button onClick={() => navigate(-1)}>이전 페이지로 가기</button>
+
+      <Button
+        props={{
+          ...PROPERTY.button.goHome,
+          handleClick: () => navigate(-1),
+        }}
+      />
     </div>
   );
 };

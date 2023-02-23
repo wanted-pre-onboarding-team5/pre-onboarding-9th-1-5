843c88c8ee9c60ae1f3ad935034bef2e34e7e5e6
eunoo1995 <eunoo1995@gmail.com>
2023-02-23 21:28:37 +0900

    [#22] Refactor: todo 페이지 todoList/TodoForm으로 컴포넌트화
    재사용의 목적이 아니더라도 불필요한 리렌더링을 방지하기 위해 컴포넌트로 분리하였습니다.

--- src/pages/Todo/index.js
+++ src/pages/Todo/index.js
@@ -1,29 +1,14 @@
-import { TodoItem } from 'components/TodoItem';
-import { useTodos } from '../../hooks/useTodos';
+import { TodoForm } from 'components/TodoForm';
+import { TodoList } from 'components/TodoList';
+import { useTodos } from 'hooks/useTodos';
 
 export const Todo = () => {
   const { todos, setTodos, inputValue, handleChange, handleSubmit } = useTodos();
 
   return (
     <>
-      <div>Todo</div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          data-testid='new-todo-input'
-          autoComplete='off'
-          onChange={handleChange}
-          value={inputValue}
-        />
-        <button type='submit' data-testid='new-todo-add-button'>
-          추가
-        </button>
-        <ul>
-          {todos
-            ? todos.map((todo) => <TodoItem key={todo.id} todo={todo} setTodos={setTodos} />)
-            : '할 일을 추가하세요.'}
-        </ul>
-      </form>
+      <TodoForm inputValue={inputValue} handleChange={handleChange} handleSubmit={handleSubmit} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </>
   );
 };

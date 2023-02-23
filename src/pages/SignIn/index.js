22b308d65b75437a30ccbd9c820040126bec192a
letsjo <hyunoh.jo@gmail.com>
2023-02-24 06:56:30 +0900

    [#21] Refactor: 상수처리 및 컴퍼넌트 호출로 구성

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,51 +1,48 @@
-import { useCheckAccount } from 'hooks/useCheckAccount';
-import { useMovePage } from 'hooks/useMovePage';
-import { postSignIn } from 'apis/loginApi';
-import { USER_TOKEN_KEY } from 'constants';
+import Button from 'components/public/Button';
+import Input from 'components/public/Input';
+import TextLink from 'components/public/TextLink';
+import { useLoginData } from 'hooks/useLoginData';
+import { PROPERTY } from 'constants';
 
 export const SignIn = () => {
   const {
-    isButtonDisabled,
-    emailRef,
-    passwordRef,
-    emailInput,
-    passwordInput,
-    handleEmailChange,
-    handlePasswordChange,
-  } = useCheckAccount();
-
-  const [goTodo, goSignUp] = useMovePage(['/todo', '/signup']);
-
-  const handleSignIn = async (e) => {
-    e.preventDefault();
-    const { accessToken } = await postSignIn({ email: emailInput, password: passwordInput });
-    if (!accessToken) return;
-    localStorage.setItem(USER_TOKEN_KEY, accessToken);
-    goTodo();
-  };
+    onChangeEmail,
+    onChangePassword,
+    userData,
+    handleSignIn,
+    handleActiveButton,
+    activeButton,
+  } = useLoginData();
 
   return (
-    <>
-      <h1>로그인</h1>
-      <form onSubmit={handleSignIn}>
-        <input
-          data-testid="email-input"
-          ref={emailRef}
-          value={emailInput}
-          onChange={handleEmailChange}
-        />
-        <input
-          data-testid="password-input"
-          ref={passwordRef}
-          value={passwordInput}
-          onChange={handlePasswordChange}
-          type="password"
-        />
-        <button type="submit" data-testid="signin-button" disabled={isButtonDisabled}>
-          로그인하기
-        </button>
-      </form>
-      <button onClick={goSignUp}>회원가입하러 가기</button>
-    </>
+    <form>
+      <div>{PROPERTY.title.signIn}</div>
+      <Input
+        props={{
+          ...PROPERTY.input.signInEmail,
+          onChange: onChangeEmail,
+        }}
+      />
+      <Input
+        props={{
+          ...PROPERTY.input.signInPassword,
+          onChange: onChangePassword,
+        }}
+      />
+      <Button
+        props={{
+          ...PROPERTY.button.signInConfirm,
+          handleClick: handleSignIn,
+          handleActive: handleActiveButton,
+          active: activeButton,
+          userData,
+        }}
+      />
+      <TextLink
+        props={{
+          ...PROPERTY.textLink.goSignUp,
+        }}
+      />
+    </form>
   );
 };
